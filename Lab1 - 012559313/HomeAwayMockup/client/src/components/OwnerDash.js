7daa309a05e3a964a59c03b854cc02cbc0d7a15b
Tong Wu <tongwu@live.com>
2018-10-07 22:06:30 -0700

    Add owner's dashboard

---
+++ Lab1 - 012559313/HomeAwayMockup/client/src/components/OwnerDash.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class OwnerDash extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bookings: []
+    }
+  }
+  componentWillMount() {
+    const token = localStorage.getItem('token')
+    axios({
+      method: 'get',
+      url: '/getBookings',
+      headers: {
+        Authorization: token,
+      }
+    }).then(res => {
+      let bookings = []
+      if (res.data.length)
+        res.data.forEach(booking => {
+          bookings.push(
+            <div key={ booking.houseId }>
+              <h3>Id of the house booked:</h3>
+              <p>{ booking.houseId }</p>
+              <h3>Start at:</h3>
+              <p>{ booking.startDate.substring(0, 10) }</p>
+              <h3>End at:</h3>
+              <p>{ booking.endDate.substring(0, 10) }</p>
+              <h3>The traveler's email is:</h3>
+              <p>{ booking.traveler }</p>
+              <hr />
+            </div>
+          )
+        })
+      else
+        bookings = ['No bookings']
+      this.setState({ bookings })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h2>Properties booked:</h2>
+        { this.state.bookings }
+      </div>
+    )
+  }
+}
