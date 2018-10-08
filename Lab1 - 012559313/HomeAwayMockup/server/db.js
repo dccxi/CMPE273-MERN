e19aa99971879223f43bd92f53f340cd4068faf4
Tong Wu <tongwu@live.com>
2018-10-07 22:12:06 -0700

    Finish server side

--- Lab1 - 012559313/HomeAwayMockup/server/db.js
+++ Lab1 - 012559313/HomeAwayMockup/server/db.js
@@ -2,6 +2,8 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import mysql from 'mysql'
 import config from './config'
+import _ from 'lodash'
+import moment from 'moment'
 
 const pool = mysql.createPool({
   connectionLimit: 100,
@@ -18,14 +20,22 @@ export function createUser(user, fail, succ) {
     else {
       let saltRounds = 12
       bcrypt.hash(user.password, saltRounds, (err, hash) => {
-        if (err) console.error(err)
+        if (err) console.error(err.sqlMessage)
         else {
           let passwordHash = hash
-          let queryString = `INSERT INTO users (EMAIL, PASSWORDHASH, FIRSTNAME, LASTNAME) VALUES (${mysql.escape(user.email)}, ${mysql.escape(passwordHash)}, ${mysql.escape(user.firstname)}, ${mysql.escape(user.lastname)});`
+          let queryString =
+            `INSERT INTO users
+             (EMAIL, PASSWORDHASH, FIRSTNAME, LASTNAME, ISOWNER)
+             VALUES (
+               ${mysql.escape(user.email)},
+               ${mysql.escape(passwordHash)},
+               ${mysql.escape(user.firstName)},
+               ${mysql.escape(user.lastName)},
+               ${mysql.escape(+user.isOwner)});`
           con.query(queryString, (err, row, fields) => {
             if (err) {
               console.error(err)
-              fail()
+              fail(err)
             } else {
               succ()
             }
@@ -41,25 +51,293 @@ export function findUser(user, fail, succ) {
   pool.getConnection((err, con) => {
     if (err) console.error(err)
     else {
-      let queryString = `SELECT * FROM users WHERE email = ${mysql.escape(user.email)};`
+      let queryString =
+        `SELECT *
+         FROM users
+         WHERE email = ${mysql.escape(user.email)};`
       con.query(queryString, (err, row, fields) => {
         if (err) {
           console.error(err)
-          fail()
+          fail(err)
         } else if (row.length > 0) {
           let { passwordHash } = row[0]
           bcrypt.compare(user.password, passwordHash, (err, isMatched) => {
             if (err) {
               console.error(err)
-              fail()
+              fail(err)
             } else if (isMatched) {
               let token = jwt.sign(user, config.secret, { expiresIn: 10800 })
               succ(token)
-            } else fail()
+            } else fail('password no match')
           })
-        } else fail()
+        } else fail('not found')
       })
       con.release()
     }
   })
+}
+
+export function getProfile(email) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `SELECT *
+           FROM users
+           WHERE email = ${mysql.escape(email)};`
+        con.query(queryString, (err, row) => {
+          if (err) {
+            rej(err)
+          } else if (row.length > 0) {
+            let ret = _.omit(row[0], ['id', 'email', 'passwordHash'])
+            res(ret)
+          }
+        })
+        con.release()
+      }
+    })
+  })
+}
+
+export function updateProfile(email, userProfile) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `UPDATE users
+              SET firstName = ${mysql.escape(userProfile.firstName)},
+                  lastName = ${mysql.escape(userProfile.lastName)},
+                  image = ${mysql.escape(userProfile.image)},
+                  phone = ${mysql.escape(userProfile.phone)},
+                  about = ${mysql.escape(userProfile.about)},
+                  city = ${mysql.escape(userProfile.city)},
+                  country = ${mysql.escape(userProfile.country)},
+                  company = ${mysql.escape(userProfile.company)},
+                  school = ${mysql.escape(userProfile.school)},
+                  hometown = ${mysql.escape(userProfile.hometown)},
+                  language = ${mysql.escape(userProfile.language)},
+                  gender = ${mysql.escape(userProfile.gender)}
+            WHERE email = ${mysql.escape(email)};`
+        con.query(queryString, (err, row) => {
+          if (err)
+            rej(err)
+          else
+            res({ success: true })
+        })
+        con.release()
+      }
+    })
+  })
+}
+
+export function createProperty(owner, property) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `INSERT INTO properties
+           SET propertyId = ${mysql.escape(property.propertyId)},
+               location = ${mysql.escape(property.location)},
+               headline = ${mysql.escape(property.headline)},
+               description = ${mysql.escape(property.description)},
+               type = ${mysql.escape(property.type)},
+               bedrooms = ${mysql.escape(property.bedrooms)},
+               accommodates = ${mysql.escape(property.accommodates)},
+               bathrooms = ${mysql.escape(property.bathrooms)},
+               bookingOption = ${mysql.escape(property.bookingOption)},
+               rate = ${mysql.escape(property.rate)},
+               minimumStay = ${mysql.escape(property.minimumStay)},
+               startDate = ${mysql.escape(property.startDate)},
+               endDate = ${mysql.escape(property.endDate)},
+               owner = ${mysql.escape(owner)};`
+        con.query(queryString, (err, row) => {
+          if (err)
+            rej(err)
+          else
+            res({ success: true })
+        })
+        con.release()
+      }
+    })
+  })
+}
+
+export function findProperty(input) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `SELECT  *
+             FROM properties
+            WHERE location
+             LIKE ${mysql.escape('%' + input.location + '%')};`
+        con.query(queryString, (err, rows) => {
+          if (err)
+            rej()
+          else if (rows.length > 0) {
+            let noMatch = false
+            for (let row of rows) {
+              let startDateInp = moment(input.startDate)
+              let endDateInp = moment(input.endDate)
+              let startDate = moment(row.startDate)
+              let endDate = moment(row.endDate)
+              // TODO： 加入已订的验证
+              if (startDateInp < startDate || endDateInp > endDate || input.accommodates > row.accommodates) {
+                noMatch = true
+              }
+            }
+            if (noMatch)
+              rej()
+            else
+              res(rows)
+          } else {
+            console.log('no found');
+          }
+        })
+        con.release()
+      }
+    })
+  })
+}
+
+export function createTrip(traveler, trip) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `INSERT INTO trips
+           SET houseId = ${mysql.escape(trip.houseId)},
+               startDate = ${mysql.escape(trip.startDate)},
+               endDate = ${mysql.escape(trip.endDate)},
+               traveler = ${mysql.escape(traveler)};`
+        con.query(queryString, (err, row) => {
+          if (err)
+            rej(err)
+          else
+            res({ success: true })
+        })
+        con.release()
+      }
+    })
+  })
+}
+
+export function getTrips(traveler) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `SELECT  *
+             FROM trips
+            WHERE traveler = ${mysql.escape(traveler)};`
+        con.query(queryString, (err, rows) => {
+          if (err)
+            rej()
+          else if (rows.length > 0) {
+            res(rows)
+          } else {
+            rej()
+          }
+        })
+        con.release()
+      }
+    })
+  })
+}
+
+export function getBookings(owner) {
+  return new Promise((res, rej) => {
+    pool.getConnection((err, con) => {
+      if (err) rej(err)
+      else {
+        let queryString =
+          `SELECT  T.*
+             FROM trips T
+            WHERE T.houseId in (
+              SELECT P.propertyId
+                FROM properties P
+               WHERE P.owner = ${mysql.escape(owner)}
+            );`
+        con.query(queryString, (err, rows) => {
+          if (err)
+            rej()
+          else if (rows.length > 0) {
+            res(rows)
+          } else {
+            rej()
+          }
+        })
+        con.release()
+      }
+    })
+  })
+}
+// SQL TESTING
+export function testWithPooling(user, fail, succ) {
+  pool.getConnection((err, con) => {
+    if (err) console.error(err)
+    else {
+      let queryString =
+        `SELECT *
+         FROM users
+         WHERE email = ${mysql.escape(user.email)};`
+      con.query(queryString, (err, row, fields) => {
+        if (err) {
+          console.error(err)
+          fail(err)
+        } else if (row.length > 0) {
+          let { passwordHash } = row[0]
+          bcrypt.compare(user.password, passwordHash, (err, isMatched) => {
+            if (err) {
+              console.error(err)
+              fail(err)
+            } else if (isMatched) {
+              let token = jwt.sign(user, config.secret, { expiresIn: 10800 })
+              succ(token)
+            } else fail('password no match')
+          })
+        } else fail('not found')
+      })
+      con.release()
+    }
+  })
+}
+
+var connection = mysql.createConnection({
+  host: config.database.host,
+  port: config.database.port,
+  user: config.database.user,
+  password: config.database.password,
+  database: config.database.name
+})
+export function testWithoutPooling(user, fail, succ) {
+  // connection.connect()
+  let queryString =
+    `SELECT *
+         FROM users
+         WHERE email = ${mysql.escape(user.email)};`
+  connection.query(queryString, (err, row, fields) => {
+    if (err) {
+      console.error(err)
+      fail(err)
+    } else if (row.length > 0) {
+      let { passwordHash } = row[0]
+      bcrypt.compare(user.password, passwordHash, (err, isMatched) => {
+        if (err) {
+          console.error(err)
+          fail(err)
+        } else if (isMatched) {
+          let token = jwt.sign(user, config.secret, { expiresIn: 10800 })
+          succ(token)
+        } else fail('password no match')
+      })
+    } else fail('not found')
+  })
+  // connection.end()
 }
--- Lab1 - 012559313/HomeAwayMockup/server/server.js
+++ Lab1 - 012559313/HomeAwayMockup/server/server.js
@@ -1,10 +1,55 @@
 import express from 'express'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
-import { createUser, findUser } from './db'
+import {
+  createUser,
+  findUser,
+  getProfile,
+  updateProfile,
+  createProperty,
+  findProperty,
+  testWithPooling,
+  testWithoutPooling,
+  createTrip,
+  getTrips,
+  getBookings
+} from './db'
+import multer from 'multer'
+import uuid from 'uuidv4'
 import { requireAuth } from './passport'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const { propertyId } = req.body
+    if (propertyId) {
+      console.log(111);
+      const propertyDir = `server/uploads/${propertyId}`
+      if (!fs.existsSync(propertyDir))
+        fs.mkdir(propertyDir, err => { console.log(err) })
+      cb(null, propertyDir)
+    } else {
+      console.log(222);
+      cb(null, 'server/uploads')
+    }
+  },
+  filename: function (req, file, cb) {
+    const { propertyId } = req.body
+    if (propertyId) {
+      console.log(333);
+      cb(null, propertyId)
+    } else {
+      console.log(444);
+      cb(null, uuid.fromString(req.user.email))
+    }
+
+  }
+})
+
+const upload = multer({ storage })
 
 app.set('port', process.env.PORT || 3001)
 
@@ -17,7 +62,7 @@ app.post('/register', (req, res) => {
   if (!user.email || !user.password) res.json({ success: false })
   else createUser(
     user,
-    () => res.json({ success: false }),
+    (err) => res.json({ success: false, error: err }),
     () => res.json({ success: true })
   )
 })
@@ -27,15 +72,98 @@ app.post('/login', (req, res) => {
   if (!user.email || !user.password) res.json({ success: false })
   else findUser(
     user,
-    () => res.json({ success: false }),
+    (err) => res.json({ success: false, error: err }),
     (token) => res.json({ success: true, token: `JWT ${token}` })
   )
 })
 
-app.get('/',
-  requireAuth,
-  (req, res) => {
-    res.send(`email: ${req.user.email}`)
-  })
+app.get('/profile', requireAuth, (req, res) => {
+  getProfile(req.user.email)
+    .then(ret => res.json(ret))
+    .catch(err => console.error(err))
+})
+
+app.post('/updateProfile', requireAuth, (req, res) => {
+  const email = req.user.email
+  const userProfile = req.body
+  updateProfile(email, userProfile)
+    .then(ret => res.json(ret))
+    .catch(err => console.error(err))
+})
+
+app.post('/postProperty', requireAuth, (req, res) => {
+  const owner = req.user.email
+  const property = req.body
+  createProperty(owner, property)
+    .then(ret => res.json(ret))
+    .catch(err => console.error(err))
+})
+
+app.post('/postImage', requireAuth, upload.single('photo'), (req, res) => {
+  res.json({ success: true })
+})
+
+app.post('/search', requireAuth, (req, res) => {
+  const input = req.body
+  findProperty(input)
+    .then(ret => res.json(ret))
+    .catch(() => res.json({ noMatch: true }))
+})
+
+app.get('/getPhoto/:id', requireAuth, (req, res) => {
+  const id = req.params.id
+  const fileLocation = path.join(__dirname + '/uploads', id, id)
+  const img = fs.readFileSync(fileLocation)
+  const base64img = new Buffer(img).toString('base64')
+  res.writeHead(200, { 'Content-Type': 'image/jpg' })
+  res.end(base64img)
+})
+
+app.post('/postTrip', requireAuth, (req, res) => {
+  const traveler = req.user.email
+  const trip = req.body
+  createTrip(traveler, trip)
+    .then(ret => res.json(ret))
+    .catch(err => console.error(err))
+})
+
+app.get('/getTrips', requireAuth, (req, res) => {
+  const traveler = req.user.email
+  getTrips(traveler)
+    .then(ret => res.json(ret))
+    .catch(err => console.error(err))
+})
+
+app.get('/getBookings', requireAuth, (req, res) => {
+  const owner = req.user.email
+  getBookings(owner)
+    .then(ret => res.json(ret))
+    .catch(err => console.error(err))
+})
+
+// TEST
+app.post('/withPooling', (req, res) => {
+  let user = req.body
+  if (!user.email || !user.password) res.json({ success: false })
+  else testWithPooling(
+    user,
+    (err) => res.json({ success: false, error: err }),
+    (token) => res.json({ success: true, token: `JWT ${token}` })
+  )
+})
+app.post('/withoutPooling', (req, res) => {
+  let user = req.body
+  if (!user.email || !user.password) res.json({ success: false })
+  else testWithoutPooling(
+    user,
+    (err) => res.json({ success: false, error: err }),
+    (token) => res.json({ success: true, token: `JWT ${token}` })
+  )
+})
+
+app.get('/checkLoggedInTest', requireAuth, (req, res) => {
+  console.log('test');
+  res.end('test')
+})
 
 app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`))
